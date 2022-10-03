ab66cbac5ffe427a8e7dacad28edfa988df8e568
softacoder <113185323+softacoder@users.noreply.github.com>
2022-10-03 14:27:55 +0100

    Add files via upload

---
+++ listoffruit.js
@@ -0,0 +1,14 @@
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+new_li.innerText = 'banana';
+
+element_list.appendChild(new_li);
+let new_li2 = document.createElement('li');
+new_li2.innerText = 'lemon';
+
+element_list.appendChild(new_li2);
+let new_li3 = document.createElement('li');
+new_li3.innerText = 'pear';
+
+element_list.appendChild(new_li3);
+
